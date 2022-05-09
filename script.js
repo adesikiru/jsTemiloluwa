323a9332a794c1690d11bd7421492620a6873077
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-09 10:41:31 +0100

    ...

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const Name = 'tOMISIN';
-alert(Name);
-console.log(Name);
+const 
+const age = 19;
+const isAge19 = true;
+/* 
+alert(Name); */
+console.log(Name + age + isAge19);
+// let my_name = 'demola';
+// console.log(my_name)
